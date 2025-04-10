dd751e5e35d1ec1fe5490af286acca7d720115c9
Duneth Widanapathirana <153067188+Dune-Wid@users.noreply.github.com>
2025-04-11 01:55:08 +0530

    Initial project setup with sample file

---
+++ src/main/java/fasttrack/FastTrackApplication.java
@@ -0,0 +1,13 @@
+package fasttrack;
+
+    public class FastTrackApplication {
+        public static void main(String[] args) {
+            System.out.println("Hello World! FastTrack Logistics System setup successful.");
+
+            // Simple Swing UI test
+            javax.swing.JOptionPane.showMessageDialog(null,
+                "Hello World! FastTrack Logistics System setup successful.",
+                "Setup Test",
+                javax.swing.JOptionPane.INFORMATION_MESSAGE);
+    }
+}
